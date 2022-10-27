302b177014223b848dd23c931ae2166c28574f1b
Juliane <julianesilvac75@gmail.com>
2022-10-27 21:54:42 +0100

    test: read_brazilian_file function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+from tests.mocks.translated_jobs import translated_job
 
 
 def test_brazilian_jobs():
-    pass
+    """Testing if the function returns a dictionary list"""
+    """with the translated keys."""
+    path = "tests/mocks/brazilians_jobs.csv"
+
+    assert read_brazilian_file(path) == translated_job
