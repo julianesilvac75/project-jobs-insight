ef238b6202b9048688aec9ab1125328a491e601a
Juliane <julianesilvac75@gmail.com>
2022-10-27 21:38:40 +0100

    test: implementing test for count_ocurrences function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,16 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    """Testing if the function returns"""
+    """the right amount of words when searching for"""
+    """Javascript and Python, and if the function is case insensitive"""
+
+    path = "src/jobs.csv"
+    python_amount = 1639
+    javascript_amount = 122
+
+    assert count_ocurrences(path, "Python") == python_amount
+    assert count_ocurrences(path, "python") == python_amount
+    assert count_ocurrences(path, "Javascript") == javascript_amount
+    assert count_ocurrences(path, "javascript") == javascript_amount
