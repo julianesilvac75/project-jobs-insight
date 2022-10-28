f0a7f64d0d11f1fa79d2506ca3de1230f3490595
Juliane <julianesilvac75@gmail.com>
2022-10-28 12:54:08 +0100

    fix: github evaluator

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -4,7 +4,7 @@ from tests.mocks.translated_jobs import translated_job
 
 def test_brazilian_jobs():
     """Testing if the function returns a dictionary list"""
-    """with the translated keys."""
+    """with the translated keys. """
     path = "tests/mocks/brazilians_jobs.csv"
 
     assert read_brazilian_file(path) == translated_job
