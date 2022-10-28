f9d034a334344ee3c3ec1ab742839e9e3f487f50
Juliane <julianesilvac75@gmail.com>
2022-10-28 14:25:46 +0100

    fix: moving mocks to tests file to fix github evaluator error

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,8 +1,65 @@
+import pytest
 from src.brazilian_jobs import read_brazilian_file
-from tests.mocks.translated_jobs import translated_job
 
 
-def test_brazilian_jobs():
+@pytest.fixture
+def translated_job():
+    return [
+        {"title": "Maquinista", "salary": "2000", "type": "trainee"},
+        {"title": "Motorista", "salary": "3000", "type": "full time"},
+        {
+            "title": "Analista de Software",
+            "salary": "4000",
+            "type": "full time",
+        },
+        {
+            "title": "Assistente administrativo",
+            "salary": "1700",
+            "type": " full time",
+        },
+        {
+            "title": "Auxiliar administrativo",
+            "salary": "1400",
+            "type": " full time",
+        },
+        {"title": "Auxiliar usinagem", "salary": "1400", "type": " full time"},
+        {
+            "title": "Auxiliar de padaria",
+            "salary": "1400",
+            "type": " full time",
+        },
+        {"title": "Analista Contábil", "salary": "1400", "type": " full time"},
+        {"title": "Gerente comercial", "salary": "5000", "type": " full time"},
+        {
+            "title": "Analista de Departamento Pessoal",
+            "salary": "4000",
+            "type": " full time",
+        },
+        {
+            "title": "Esportista de Futebol profissional",
+            "salary": "50000",
+            "type": " full time",
+        },
+        {
+            "title": "Analista de crédito",
+            "salary": "4000",
+            "type": " full time",
+        },
+        {
+            "title": "Pessoa Programadora",
+            "salary": "3000",
+            "type": " full time",
+        },
+        {"title": "Ux Designer", "salary": "3000", "type": " full time"},
+        {
+            "title": "Auxiliar de manutenção",
+            "salary": " 1400",
+            "type": " full time",
+        },
+    ]
+
+
+def test_brazilian_jobs(translated_job):
     """Testing if the function returns a dictionary list"""
     """with the translated keys. """
     path = "tests/mocks/brazilians_jobs.csv"
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,9 +1,4 @@
 import pytest
-from tests.mocks.jobs_by_criteria import (
-    jobs_by_date_posted,
-    jobs_by_max_salary,
-    jobs_by_min_salary,
-)
 from src.sorting import sort_by
 
 
@@ -17,15 +12,75 @@ def jobs():
     ]
 
 
-def test_sort_by_criteria(jobs):
+@pytest.fixture
+def jobs_by_criteria():
+    return {
+        "min_salary": [
+            {
+                "min_salary": "0",
+                "max_salary": "10",
+                "date_posted": "2020-05-08",
+            },
+            {
+                "min_salary": "10",
+                "max_salary": "100",
+                "date_posted": "2020-05-09",
+            },
+            {
+                "min_salary": "50",
+                "max_salary": "500",
+                "date_posted": "2020-05-06",
+            },
+            {"min_salary": "", "max_salary": "", "date_posted": "2020-05-07"},
+        ],
+        "max_salary": [
+            {
+                "min_salary": "50",
+                "max_salary": "500",
+                "date_posted": "2020-05-06",
+            },
+            {
+                "min_salary": "10",
+                "max_salary": "100",
+                "date_posted": "2020-05-09",
+            },
+            {
+                "min_salary": "0",
+                "max_salary": "10",
+                "date_posted": "2020-05-08",
+            },
+            {"min_salary": "", "max_salary": "", "date_posted": "2020-05-07"},
+        ],
+        "date_posted": [
+            {
+                "min_salary": "10",
+                "max_salary": "100",
+                "date_posted": "2020-05-09",
+            },
+            {
+                "min_salary": "0",
+                "max_salary": "10",
+                "date_posted": "2020-05-08",
+            },
+            {"min_salary": "", "max_salary": "", "date_posted": "2020-05-07"},
+            {
+                "min_salary": "50",
+                "max_salary": "500",
+                "date_posted": "2020-05-06",
+            },
+        ],
+    }
+
+
+def test_sort_by_criteria(jobs, jobs_by_criteria):
     """Testing if the function returns a list with jobs"""
     """sorted by the right criteria. """
 
     sort_by(jobs, "min_salary")
-    assert jobs == jobs_by_min_salary
+    assert jobs == jobs_by_criteria["min_salary"]
 
     sort_by(jobs, "max_salary")
-    assert jobs == jobs_by_max_salary
+    assert jobs == jobs_by_criteria["max_salary"]
 
     sort_by(jobs, "date_posted")
-    assert jobs == jobs_by_date_posted
+    assert jobs == jobs_by_criteria["date_posted"]
